325a52ff12b7064b943c08dae24b256c5b2e2f74
hovercats <hovercatswithlasereyes@protonmail.com>
2024-02-08 23:37:11 +0100

    tz: sync with upstream
    details here: https://github.com/oasislinux/oasis/pull/92#issuecomment-1933372162

--- pkg/tz/tzdir.h
+++ pkg/tz/tzdir.h
@@ -1,6 +1,2 @@
-#ifndef TZDEFAULT
-# define TZDEFAULT "/etc/localtime" /* default zone */
-#endif
-#ifndef TZDIR
-# define TZDIR "/usr/share/zoneinfo" /* TZif directory */
-#endif
+#define TZDEFAULT "/etc/localtime" /* default zone */
+#define TZDIR "/share/zoneinfo" /* TZif directory */
