4c9514e9fcf8a2b79e0121519567f87fd72e9b40
hovercats <hovercatswithlasereyes@protonmail.com>
2024-02-10 02:54:45 +0100

    eiwd: build libell-internal.a. this should be more correct

--- pkg/eiwd/gen.lua
+++ pkg/eiwd/gen.lua
@@ -14,6 +14,33 @@ pkg.deps = {
 	'pkg/linux-headers/headers',
 }
 
+lib('libell-internal.a', [[
+	ell/(
+		util.c test.c strv.c
+		utf8.c queue.c hashmap.c
+		string.c settings.c main.c
+		idle.c signal.c timeout.c
+		io.c ringbuf.c log.c
+		checksum.c netlink.c genl.c
+		rtnl.c dbus.c dbus-message.c
+		dbus-util.c dbus-service.c
+		dbus-client.c dbus-name-cache.c
+		dbus-filter.c gvariant-util.c
+		siphash.c hwdb.c cipher.c random.c
+		uintset.c base64.c pem.c tls.c
+		tls-record.c tls-suites.c
+		tls-extensions.c uuid.c
+		key.c file.c dir.c net.c
+		dhcp.c dhcp-transport.c
+		dhcp-lease.c dhcp-util.c
+		dhcp-server.c cert.c cert-crypto.c
+		ecc-external.c ecc.c ecdh.c time.c
+		path.c dhcp6.c icmp6.c dhcp6-lease.c
+		dhcp6-transport.c acd.c netconfig.c
+		sysctl.c
+	)
+]])
+
 exe('iwd', [[
 	src/(
 		main.c netdev.c wiphy.c
@@ -46,30 +73,7 @@ exe('iwd', [[
 
 		ofono.c
 	)
-	ell/(
-		util.c test.c strv.c
-		utf8.c queue.c hashmap.c
-		string.c settings.c main.c
-		idle.c signal.c timeout.c
-		io.c ringbuf.c log.c
-		checksum.c netlink.c genl.c
-		rtnl.c dbus.c dbus-message.c
-		dbus-util.c dbus-service.c
-		dbus-client.c dbus-name-cache.c
-		dbus-filter.c gvariant-util.c
-		siphash.c hwdb.c cipher.c random.c
-		uintset.c base64.c pem.c tls.c
-		tls-record.c tls-suites.c
-		tls-extensions.c uuid.c
-		key.c file.c dir.c net.c
-		dhcp.c dhcp-transport.c
-		dhcp-lease.c dhcp-util.c
-		dhcp-server.c cert.c cert-crypto.c
-		ecc-external.c ecc.c ecdh.c time.c
-		path.c dhcp6.c icmp6.c dhcp6-lease.c
-		dhcp6-transport.c acd.c netconfig.c
-		sysctl.c
-	)
+		libell-internal.a
 ]])
 
 file('bin/iwd', '755', '$outdir/iwd')
