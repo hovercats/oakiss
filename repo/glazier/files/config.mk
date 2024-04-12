39e0db68692b57050cc76f026bf6245eaf224832
hovercats <hovercatswithlasereyes@protonmail.com>
2024-04-12 12:52:23 +0200

    glazier: fix build

--- repo/glazier/files/config.mk
+++ repo/glazier/files/config.mk
@@ -5,6 +5,6 @@ PREFIX = /usr/local
 MANDIR = ${PREFIX}/man
 
 CPPFLAGS = -I./libwm
-CFLAGS += -Wall -Wextra -pedantic
+CFLAGS += -Wall -Wextra -pedantic -I/usr/include
 LDFLAGS += -L./libwm ${LIBS} -static
 LIBS = -lwm -lxcb -lXau -lxcb-cursor -lxcb-render -lxcb-render-util -lxcb-image -lxcb-shm -lxcb-util -lxcb-randr
