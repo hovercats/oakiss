d82c1315b0e8e98d4f0ddfdade1f5176cfa2f258
hovercats <hovercatswithlasereyes@protonmail.com>
2024-11-26 03:31:26 +0100

    dtach: port to oasis

---
+++ pkg/dtach/gen.lua
@@ -0,0 +1,16 @@
+cflags{
+	'-I$dir',
+	'-pedantic',
+	'-Wall',
+	'-Wextra',
+}
+
+exe('dtach', {
+	'main.c',
+	'master.c',
+	'attach.c',
+})
+
+file('bin/dtach', '755', '$outdir/dtach')
+
+man{'dtach.1'}
