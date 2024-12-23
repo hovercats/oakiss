e90a03da48d805ffb71c408d959b6e345e5569ca
hovercats <hovercatswithlasereyes@protonmail.com>
2024-12-23 01:11:49 +0100

    xxhash: port to oasis

---
+++ pkg/xxhash/gen.lua
@@ -0,0 +1,25 @@
+cflags{
+	'-std=c99',
+	'-pedantic',
+	'-Wall',
+}
+
+pkg.hdrs = copy('$outdir/include', '$srcdir', {'xxh3.h', 'xxhash.h'})
+pkg.hdrs.install = true
+
+lib('libxxhash.a', {'xxhash.c'})
+
+exe('xxhsum', [[
+	cli/(
+		xxhsum.c xsum_os_specific.c
+		xsum_output.c xsum_sanity_check.c
+		xsum_bench.c
+	)
+	libxxhash.a
+]])
+
+file('lib/libxxhash.a', '644', '$outdir/libxxhash.a')
+file('bin/xxhsum', '755', '$outdir/xxhsum')
+man{'cli/xxhsum.1'}
+
+fetch 'git'
