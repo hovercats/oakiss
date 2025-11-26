dfdb88e0df8b02da352b7155d44f6a152d1392d9
hovercats <hovercatswithlasereyes@protonmail.com>
2025-11-26 07:58:09 +0100

    bc: build dc too

--- pkg/bc/gen.lua
+++ pkg/bc/gen.lua
@@ -5,14 +5,27 @@ cflags{
 	'-I $srcdir/h',
 }
 
+lib('libbc.a', [[lib/(getopt.c getopt1.c vfprintf.c number.c)]])
+
 exe('bin/bc', [[
 	bc/(
 		main.c bc.c scan.c execute.c load.c storage.c util.c global.c
 		warranty.c
 	)
-	lib/number.c
+	$outdir/libbc.a
 ]])
+
+exe('bin/dc', [[
+	dc/(
+		dc.c misc.c eval.c stack.c array.c numeric.c string.c
+	)
+	$outdir/libbc.a
+]])
+
 file('bin/bc', '755', '$outdir/bin/bc')
+file('bin/dc', '755', '$outdir/bin/dc')
+
 man{'doc/bc.1'}
+man{'doc/dc.1'}
 
 fetch 'curl'
