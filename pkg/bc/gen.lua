70e6bf71c0b7d4ccdf94952c898001d13ca2b06e
hovercats <hovercatswithlasereyes@protonmail.com>
2026-01-21 17:55:16 +0100

    bc: dont build dc, as its now provided by sbase instead

--- pkg/bc/gen.lua
+++ pkg/bc/gen.lua
@@ -5,27 +5,16 @@ cflags{
 	'-I $srcdir/h',
 }
 
-lib('libbc.a', [[lib/(getopt.c getopt1.c vfprintf.c number.c)]])
-
 exe('bin/bc', [[
 	bc/(
 		main.c bc.c scan.c execute.c load.c storage.c util.c global.c
 		warranty.c
 	)
-	$outdir/libbc.a
-]])
-
-exe('bin/dc', [[
-	dc/(
-		dc.c misc.c eval.c stack.c array.c numeric.c string.c
-	)
-	$outdir/libbc.a
+	lib/number.c
 ]])
 
 file('bin/bc', '755', '$outdir/bin/bc')
-file('bin/dc', '755', '$outdir/bin/dc')
 
 man{'doc/bc.1'}
-man{'doc/dc.1'}
 
 fetch 'curl'
