a0c9c60a7521275f73f38023ad0a8ca42a9d220e
hovercats <hovercatswithlasereyes@protonmail.com>
2026-03-09 13:30:24 +0100

    tohu: add

---
+++ pkg/tohu/config.h
@@ -0,0 +1,45 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <xkbcommon/xkbcommon-keysyms.h>
+#include "types.h"
+#include "tohu.h"
+static const struct config cfg = {
+	.motion_throttle_hz = 85,
+	.border_col_active = 0xdeadca7,
+	.border_col_normal = 0xff444444,
+	.border_width = 3,
+	.gaps = 0,
+};
+
+static const char* termcmd[] = { "st", NULL };
+static const char* menucmd[] = { "dmenu_run", NULL };
+static struct bind binds[] = {
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_Return, { .v = termcmd }, spawn },
+	{ SWC_BINDING_KEY,    MOD1,       XKB_KEY_p,      { .v = menucmd }, spawn },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_q,      { .v = NULL },    quit },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_Tab,    { .v = NULL },    focus_next },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_f,      { .v = NULL },    fullscreen },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_q,      { .v = NULL },    kill_sel },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_1,      { .ui = 1 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_2,      { .ui = 2 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_3,      { .ui = 3 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_4,      { .ui = 4 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_5,      { .ui = 5 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_6,      { .ui = 6 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_7,      { .ui = 7 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_8,      { .ui = 8 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1,        XKB_KEY_9,      { .ui = 9 },      workspace_goto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_1,      { .ui = 1 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_2,      { .ui = 2 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_3,      { .ui = 3 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_4,      { .ui = 4 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_5,      { .ui = 5 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_6,      { .ui = 6 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_7,      { .ui = 7 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_8,      { .ui = 8 },      workspace_moveto },
+	{ SWC_BINDING_KEY,    MOD1|SHFT,   XKB_KEY_9,      { .ui = 9 },      workspace_moveto },
+	{ SWC_BINDING_BUTTON, MOD1,        BTN_LEFT,       { .v = NULL },    mouse_move },
+	{ SWC_BINDING_BUTTON, MOD1,        BTN_RIGHT,      { .v = NULL },    mouse_resize },
+};
+
+#endif /* CONFIG_H */
---
+++ pkg/tohu/gen.lua
@@ -0,0 +1,44 @@
+cflags{
+	'-std=c99',
+	'-D _POSIX_C_SOURCE=200809L',
+	'-I $dir',
+	'-I $srcdir/source/include',
+	'-isystem $builddir/pkg/fontconfig/include',
+	'-isystem $builddir/pkg/libdrm/include',
+	'-isystem $builddir/pkg/libinput/include',
+	'-isystem $builddir/pkg/libxkbcommon/include',
+	'-isystem $builddir/pkg/swc/include',
+	'-isystem $builddir/pkg/wld/include',
+	'-isystem $builddir/pkg/pixman/include',
+	'-isystem $builddir/pkg/wayland-protocols/include',
+	'-isystem $builddir/pkg/wayland/include',
+}
+
+pkg.deps = {
+	'pkg/fontconfig/headers',
+	'pkg/libdrm/headers',
+	'pkg/libinput/headers',
+	'pkg/libxkbcommon/headers',
+	'pkg/swc/headers',
+	'pkg/wld/headers',
+	'pkg/pixman/headers',
+	'pkg/wayland/headers',
+	'pkg/wayland-protocols/headers',
+}
+
+exe('tohu', {
+	'source/tohu.c',
+	'source/util.c',
+	'$builddir/pkg/swc/libswc.a',
+	'$builddir/pkg/wld/libwld.a',
+	'$builddir/pkg/pixman/libpixman.a',
+	'$builddir/pkg/libinput/libinput.a.d',
+	'$builddir/pkg/libxkbcommon/libxkbcommon.a',
+	'$builddir/pkg/libdrm/libdrm.a',
+	'$builddir/pkg/wayland/libwayland-server.a.d',
+	'$builddir/pkg/fontconfig/libfontconfig.a.d',
+})
+
+file('bin/tohu', '755', '$outdir/tohu')
+
+fetch 'git'
