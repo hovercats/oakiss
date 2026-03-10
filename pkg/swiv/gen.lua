fa3ac60b7aa0cb4c077be8301ee655a35738857f
hovercats <hovercatswithlasereyes@protonmail.com>
2026-03-10 12:49:06 +0100

    swiv: add

---
+++ pkg/swiv/gen.lua
@@ -0,0 +1,35 @@
+cflags{
+	'-std=c99',
+	'-I $outdir',
+	'-isystem $builddir/pkg/fontconfig/include',
+	'-isystem $builddir/pkg/pixman/include',
+	'-isystem $builddir/pkg/wayland/include',
+	'-isystem $builddir/pkg/wld/include',
+}
+
+pkg.deps = {
+	'$outdir/protocol/xdg-shell-client-protocol.h',
+	'pkg/fontconfig/headers',
+	'pkg/pixman/headers',
+	'pkg/wayland/headers',
+	'pkg/wld/headers',
+}
+
+waylandproto('protocol/xdg-shell.xml', {
+	client='protocol/xdg-shell-client-protocol.h',
+	code='protocol/xdg-shell.c'
+})
+
+exe('swiv', {
+	'swiv.c',
+	'image.c',
+	'$outdir/protocol/xdg-shell.c.o',
+	'$builddir/pkg/pixman/libpixman.a',
+	'$builddir/pkg/wld/libwld.a.d',
+	'$builddir/pkg/wayland/libwayland-client.a.d',
+	'$builddir/pkg/fontconfig/libfontconfig.a.d',
+})
+
+file('bin/swiv', '755', '$outdir/swiv')
+
+fetch 'git'
