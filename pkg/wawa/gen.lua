bf834584c1054a054b6bde1a0530c44fdf6f82b4
hovercats <hovercatswithlasereyes@protonmail.com>
2026-07-29 21:47:22 +0200

    wawa: create the wlr-layer-shell header instead

--- pkg/wawa/gen.lua
+++ pkg/wawa/gen.lua
@@ -6,13 +6,17 @@ cflags{
 	'-D _POSIX_C_SOURCE=200809L',
 	'-I $outdir/include',
 	'-isystem $builddir/pkg/linux-headers/include',
-	'-isystem $builddir/pkg/neuswc/include',
 	'-isystem $builddir/pkg/wayland/include',
 	'-isystem $builddir/pkg/wayland-protocols/include',
 }
 
+waylandproto('wlr-layer-shell-unstable-v1.xml', {
+	client='include/wlr-layer-shell-unstable-v1-protocol.h',
+	code='wlr-layer-shell-unstable-v1.c',
+})
+
 pkg.deps = {
-	'pkg/neuswc/headers',
+	'$outdir/include//wlr-layer-shell-unstable-v1-protocol.h',
 	'pkg/linux-headers/headers',
 	'pkg/wayland/headers',
 	'pkg/wayland-protocols/headers',
@@ -20,7 +24,7 @@ pkg.deps = {
 
 exe('wawa', {
 	'wawa.c',
-	'$builddir/pkg/neuswc/wlr-layer-shell-unstable-v1.c.o',
+	'wlr-layer-shell-unstable-v1.c.o',
 	'$builddir/pkg/wayland-protocols/xdg-shell-protocol.c.o',
 	'$builddir/pkg/wayland/libwayland-client.a.d',
 	'$builddir/pkg/wayland/libwayland-server.a.d',
