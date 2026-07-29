57fc74a01c5ccf0a2ed01c463898386bdc93c658
hovercats <hovercatswithlasereyes@protonmail.com>
2026-07-29 17:19:27 +0200

    uxn12: bump to latest

--- pkg/uxn12/gen.lua
+++ pkg/uxn12/gen.lua
@@ -2,12 +2,21 @@ cflags{
 	'-std=c99',
 	'-pedantic',
 	'-D _POSIX_C_SOURCE=199309L',
+	'-I $outdir/include',
+	'-isystem $builddir/pkg/alsa-lib/include',
 	'-isystem $builddir/pkg/libxkbcommon/include',
 	'-isystem $builddir/pkg/wayland/include',
 	'-isystem $builddir/pkg/wayland-protocols/include',
 }
 
+waylandproto('src/wlr-layer-shell-unstable-v1.xml', {
+	client='include/wlr-layer-shell-client-protocol.h',
+	code='wlr-layer-shell-client-protocol.c',
+})
+
 pkg.deps = {
+	'$outdir/include/wlr-layer-shell-client-protocol.h',
+	'pkg/alsa-lib/headers',
 	'pkg/libxkbcommon/headers',
 	'pkg/wayland/headers',
 	'pkg/wayland-protocols/headers',
@@ -15,7 +24,9 @@ pkg.deps = {
 
 exe('uxn12', {
 	'src/uxn12.c',
-	'src/xdg-shell-protocol.c',
+	'wlr-layer-shell-client-protocol.c.o',
+	'$builddir/pkg/wayland-protocols/xdg-shell-protocol.c.o',
+	'$builddir/pkg/alsa-lib/libasound.a',
 	'$builddir/pkg/libxkbcommon/libxkbcommon.a',
 	'$builddir/pkg/wayland/libwayland-client.a.d',
 })
